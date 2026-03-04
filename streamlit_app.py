755d9e06db6d7a71887b7837c86e7b4b26b3db7e
Viso V <161427857+viso27@users.noreply.github.com>
2026-03-04 11:03:26 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import requests
+
+st.title("🌦 Weather App")
+
+city = st.text_input("Enter City Name")
+
+API_KEY = "4d3a1522de33d815285ebc73b0f3441b"
+
+if st.button("Get Weather"):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] == 200:
+        temp = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        weather = data["weather"][0]["description"]
+
+        st.success(f"Temperature: {temp} °C")
+        st.info(f"Humidity: {humidity}%")
+        st.write(f"Weather: {weather}")
+    else:
+        st.error("City not found")
